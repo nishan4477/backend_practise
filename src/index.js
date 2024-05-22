900d8a1733e247f07bab690e46e2a4ca7ace1467
nishan4477 <nishansakha@gmail.com>
2024-05-22 12:39:14 +0545

    refresh and acess token was added to the user schema

--- src/index.js
+++ src/index.js
@@ -8,4 +8,12 @@ dotenv.config({
 
 const app = express();
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("server is running on port 8000");
+    });
+  })
+  .catch((err) => {
+    console.log("failed to connect to the database");
+  });
---
+++ src/models/video.model.js
@@ -0,0 +1,34 @@
+import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        videoFile:{
+            type:String,  //cloudainary
+            required:true
+        },
+        description:{
+            type:String,  //cloudainary
+            required:true
+        },
+        duration:{
+            type:Number,
+            default:0,
+
+        },
+        isPublished:{
+            type:true,
+            default:true
+        },
+        owner:{
+            type:Schema.Types.ObjectId,
+            ref:'User'
+        }
+    },{
+        timestamps:true
+    }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model('Video', videoSchema);
